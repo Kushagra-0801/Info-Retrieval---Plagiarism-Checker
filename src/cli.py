e6a69a6fd76c6b3b7e5ff9b7e669155ce7bb8835
Kushagra Gupta <kushgpt08@gmail.com>
2020-10-12 09:34:13 +0530

    Add output format specifier to CLI

--- src/cli.py
+++ src/cli.py
@@ -1,12 +1,19 @@
 import argparse
 
 _parser = argparse.ArgumentParser(description='Check for plagiarism in your documents')
-_group = _parser.add_mutually_exclusive_group(required=True)
-_group.add_argument('-f', '--file', type=str, help='Open the given file and check for plagiarism')
-_group.add_argument('-i', '--index', type=str, help='Index the given directory for use as corpus')
+_commands = _parser.add_subparsers(required=True)
+
+_query = _commands.add_parser('query', help='Check file for plagiarism')
+_query.add_argument('file', type=str, help='Check this file (or all files in this folder)')
+_output_formats = _query.add_mutually_exclusive_group(required=True)
+_output_formats.add_argument('-t', '--table', default=True, action='store_true', help='Print output in tabular format')
+_output_formats.add_argument('-j', '--json', action='store_true', help='Print output in json format')
+
+_index = _commands.add_parser('index', help='Make index')
+_index.add_argument('index', type=str, help='Index this directory')
 
 """
-<script> [-h] (-f FILE | -i INDEX)
+<script> [-h] (query FILE [-j | -t] | index INDEX)
 file  => FILE -> (file | folder containing files) that need to checked for plagiarism
 index => INDEX -> folder with the files that will constitute the corpus
 """
