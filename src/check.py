33645d075d8fe4b08e1b7337ea01c3639ab4646a
Kushagra Gupta <kushgpt08@gmail.com>
2020-10-16 23:05:37 +0530

    Rebase branch and do the requested changes
    Added some doc comments and fix all other errors and such too

--- src/check.py
+++ src/check.py
@@ -1,26 +1,33 @@
-"""
-This file includes stuff actual checking
-"""
-from typing import List
-from math import log10 as log
+from math import log10
+from typing import Dict
+
+import utils
+from .make_index import Index
 
 
-class PlagiarismCheck:
-    def __init__(self, index: Index, query_path: str):
+class PlagiarismChecker:
+    """
+    Checks files for plagiarism w.r.t to the index given during initialization
+    """
+
+    def __init__(self, index: Index):
         self.index = index
-        self.query_path = query_path
 
-    # Returns score of query file, matching with each training file
-    def find_score(self) -> List[float]:
-        score_list = list()
-        total_docs = self.index.term_frequency.size()
-        tokens = utils.get_tokens(self.query_path)
-        for tf_map in self.index.term_frequency:
+    def find_score(self, contents: str) -> Dict[str, float]:
+        """
+        Check file contents for plagiarism
+        :param contents: The file contents to check for plagiarism
+        :return: Dictionary of plagiarism score w.r.t each original document
+        """
+        score_list = {}
+        total_docs = len(self.index.term_freqs)
+        tokens = utils.tokenize(contents)
+        for document, tf_map in self.index.term_freqs.items():
             score = 0.0
             for token in tokens:
                 if token in tf_map:
-                    term_weight = 1.0 + log(tf_map[token])
-                    doc_freq_weight = log(total_docs / self.index.doc_frequency[token])
+                    term_weight = 1.0 + log10(tf_map[token])
+                    doc_freq_weight = log10(total_docs / self.index.doc_freq[token])
                     score += term_weight * doc_freq_weight
-            score_list.append(score)
+            score_list[document] = score
         return score_list
