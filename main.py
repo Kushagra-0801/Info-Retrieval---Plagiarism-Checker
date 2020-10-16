7fbafac6e7436b0ebfa95f89999debc13fbfb749
Kushagra Gupta <kushgpt08@gmail.com>
2020-10-16 22:38:17 +0530

    Refactor file reading + read file contents
    Just came to know that Path objects have a dedicated read_text() method

--- main.py
+++ main.py
@@ -10,26 +10,38 @@ def gen_index(path: Path) -> Index:
     for file in path.iterdir():
         if not file.is_file():
             continue
-        with open(file) as f:
-            contents = f.read()
-            index.add_doc(file, contents)
+        contents = file.read_text()
+        index.add_doc(file, contents)
     return index
 
 
-args = PARSER.parse_args()
+def main():
+    args = PARSER.parse_args()
 
-if args.index:
-    p = Path(args.index)
-    if not p.is_dir():
-        print("Index path must be a directory containing the corpus files")
-        exit(1)
-    index = gen_index(p)
-    with open('index.pk', 'wb+') as f:
-        pickle.dump(index, f)
-else:
-    # TODO: Write the case with the query
-    # 3 cases
-    # 1. stdin
-    # 2. Single file
-    # 3. Directory of files
-    pass
+    if args.index:
+        p = Path(args.index)
+        if not p.is_dir():
+            print("Index path must be a directory containing the corpus files")
+            exit(1)
+        index = gen_index(p)
+        with open('index.pk', 'wb+') as f:
+            pickle.dump(index, f)
+    else:
+        contents = {}
+        if args.file == '-':
+            from sys import stdin
+            contents['stdin'] = stdin.read()
+        else:
+            p = Path(args.file)
+            if p.is_file():
+                contents[p] = p.read_text()
+            elif p.is_dir():
+                for file in p.iterdir():
+                    if not file.is_file():
+                        continue
+                    contents[file] = file.read_text()
+        # contents has the file contents now
+        # just need to do the plagiarism check now
+
+
+main()
