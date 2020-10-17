7b6adcabaaccd9e531c7fefea1c15ebb22a8b85c
Kushagra Gupta <kushgpt08@gmail.com>
2020-10-17 21:55:46 +0530

    remove json

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 import argparse
-import json
 import pickle
-from pathlib import Path, PosixPath
+from pathlib import Path
 from sys import stdin, stderr
-from typing import Any
 
 from check import PlagiarismChecker
 from make_index import Index
 from tabulate import tabulate
 
 THRESHOLD = 0.6
-
-
-class PathEncoder(json.JSONEncoder):
-    def default(self, o: Any) -> Any:
-        if isinstance(o, (Path, PosixPath)):
-            return o.name
-        return json.JSONEncoder.default(self, o)
 
 
 def gen_index(args):
@@ -62,10 +53,7 @@ def check_for_plagiarism(args):
         scores = checker.find_score(contents)
         data[file] = sorted([(original, score) for original, score in scores.items() if score > THRESHOLD],
                             reverse=True, key=lambda x: x[1])
-    if args.json:
-        print(json.dumps(data, cls=PathEncoder))
-    elif args.table:
-        tabulate(data)
+    tabulate(data)
 
 
 def main():
@@ -74,10 +62,6 @@ def main():
 
     query = commands.add_parser('query', help='Check file for plagiarism')
     query.add_argument('file', type=str, help='Check this file (or all files in this folder)')
-    output_formats = query.add_mutually_exclusive_group(required=True)
-    output_formats.add_argument('-t', '--table', default=True, action='store_true',
-                                help='Print output in tabular format')
-    output_formats.add_argument('-j', '--json', action='store_true', help='Print output in json format')
     query.set_defaults(func=check_for_plagiarism)
 
     index = commands.add_parser('index', help='Make index')
